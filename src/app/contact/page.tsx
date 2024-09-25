b386045f06ed8e45a4a9a211bb834d0643a23764
sauravbag101 <sauravbag101@gmail.com>
2024-09-25 15:29:38 +0530

    hello ther

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -8,31 +8,31 @@ function Page() {
           {/* <svg className="w-36" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 84 21" preserveAspectRatio="xMinYMin meet" version="1.1" focusable={false}>
             <g className="inbug" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
               <path
-                d="M19.479,0 L1.583,0 C0.727,0 0,0.677 0,1.511 L0,19.488 C0,20.323 0.477,21 1.333,21 L19.229,21 C20.086,21 21,20.323 21,19.488 L21,1.511 C21,0.677 20.336,0 19.479,0"
-                className="bug-text-color" transform="translate(63.000000, 0.000000)"></path>
+                d="M19.479&apos0 L1.583&apos0 C0.727&apos0 0&apos0.677 0&apos1.511 L0&apos19.488 C0&apos20.323 0.477&apos21 1.333&apos21 L19.229&apos21 C20.086&apos21 21&apos20.323 21&apos19.488 L21&apos1.511 C21&apos0.677 20.336&apos0 19.479&apos0"
+                className="bug-text-color" transform="translate(63.000000&apos 0.000000)"></path>
               <path
-                d="M82.479,0 L64.583,0 C63.727,0 63,0.677 63,1.511 L63,19.488 C63,20.323 63.477,21 64.333,21 L82.229,21 C83.086,21 84,20.323 84,19.488 L84,1.511 C84,0.677 83.336,0 82.479,0 Z M71,8 L73.827,8 L73.827,9.441 L73.858,9.441 C74.289,8.664 75.562,7.875 77.136,7.875 C80.157,7.875 81,9.479 81,12.45 L81,18 L78,18 L78,12.997 C78,11.667 77.469,10.5 76.227,10.5 C74.719,10.5 74,11.521 74,13.197 L74,18 L71,18 L71,8 Z M66,18 L69,18 L69,8 L66,8 L66,18 Z M69.375,4.5 C69.375,5.536 68.536,6.375 67.5,6.375 C66.464,6.375 65.625,5.536 65.625,4.5 C65.625,3.464 66.464,2.625 67.5,2.625 C68.536,2.625 69.375,3.464 69.375,4.5 Z"
+                d="M82.479&apos0 L64.583&apos0 C63.727&apos0 63&apos0.677 63&apos1.511 L63&apos19.488 C63&apos20.323 63.477&apos21 64.333&apos21 L82.229&apos21 C83.086&apos21 84&apos20.323 84&apos19.488 L84&apos1.511 C84&apos0.677 83.336&apos0 82.479&apos0 Z M71&apos8 L73.827&apos8 L73.827&apos9.441 L73.858&apos9.441 C74.289&apos8.664 75.562&apos7.875 77.136&apos7.875 C80.157&apos7.875 81&apos9.479 81&apos12.45 L81&apos18 L78&apos18 L78&apos12.997 C78&apos11.667 77.469&apos10.5 76.227&apos10.5 C74.719&apos10.5 74&apos11.521 74&apos13.197 L74&apos18 L71&apos18 L71&apos8 Z M66&apos18 L69&apos18 L69&apos8 L66&apos8 L66&apos18 Z M69.375&apos4.5 C69.375&apos5.536 68.536&apos6.375 67.5&apos6.375 C66.464&apos6.375 65.625&apos5.536 65.625&apos4.5 C65.625&apos3.464 66.464&apos2.625 67.5&apos2.625 C68.536&apos2.625 69.375&apos3.464 69.375&apos4.5 Z"
                 className="background" fill="#0a66c2"></path>
             </g>
             <g className="linkedin-text">
               <path
-                d="M60,18 L57.2,18 L57.2,16.809 L57.17,16.809 C56.547,17.531 55.465,18.125 53.631,18.125 C51.131,18.125 48.978,16.244 48.978,13.011 C48.978,9.931 51.1,7.875 53.725,7.875 C55.35,7.875 56.359,8.453 56.97,9.191 L57,9.191 L57,3 L60,3 L60,18 Z M54.479,10.125 C52.764,10.125 51.8,11.348 51.8,12.974 C51.8,14.601 52.764,15.875 54.479,15.875 C56.196,15.875 57.2,14.634 57.2,12.974 C57.2,11.268 56.196,10.125 54.479,10.125 L54.479,10.125 Z"
+                d="M60&apos18 L57.2&apos18 L57.2&apos16.809 L57.17&apos16.809 C56.547&apos17.531 55.465&apos18.125 53.631&apos18.125 C51.131&apos18.125 48.978&apos16.244 48.978&apos13.011 C48.978&apos9.931 51.1&apos7.875 53.725&apos7.875 C55.35&apos7.875 56.359&apos8.453 56.97&apos9.191 L57&apos9.191 L57&apos3 L60&apos3 L60&apos18 Z M54.479&apos10.125 C52.764&apos10.125 51.8&apos11.348 51.8&apos12.974 C51.8&apos14.601 52.764&apos15.875 54.479&apos15.875 C56.196&apos15.875 57.2&apos14.634 57.2&apos12.974 C57.2&apos11.268 56.196&apos10.125 54.479&apos10.125 L54.479&apos10.125 Z"
                 fill="#0a66c2"></path>
               <path
-                d="M47.6611,16.3889 C46.9531,17.3059 45.4951,18.1249 43.1411,18.1249 C40.0001,18.1249 38.0001,16.0459 38.0001,12.7779 C38.0001,9.8749 39.8121,7.8749 43.2291,7.8749 C46.1801,7.8749 48.0001,9.8129 48.0001,13.2219 C48.0001,13.5629 47.9451,13.8999 47.9451,13.8999 L40.8311,13.8999 L40.8481,14.2089 C41.0451,15.0709 41.6961,16.1249 43.1901,16.1249 C44.4941,16.1249 45.3881,15.4239 45.7921,14.8749 L47.6611,16.3889 Z M45.1131,11.9999 C45.1331,10.9449 44.3591,9.8749 43.1391,9.8749 C41.6871,9.8749 40.9121,11.0089 40.8311,11.9999 L45.1131,11.9999 Z"
+                d="M47.6611&apos16.3889 C46.9531&apos17.3059 45.4951&apos18.1249 43.1411&apos18.1249 C40.0001&apos18.1249 38.0001&apos16.0459 38.0001&apos12.7779 C38.0001&apos9.8749 39.8121&apos7.8749 43.2291&apos7.8749 C46.1801&apos7.8749 48.0001&apos9.8129 48.0001&apos13.2219 C48.0001&apos13.5629 47.9451&apos13.8999 47.9451&apos13.8999 L40.8311&apos13.8999 L40.8481&apos14.2089 C41.0451&apos15.0709 41.6961&apos16.1249 43.1901&apos16.1249 C44.4941&apos16.1249 45.3881&apos15.4239 45.7921&apos14.8749 L47.6611&apos16.3889 Z M45.1131&apos11.9999 C45.1331&apos10.9449 44.3591&apos9.8749 43.1391&apos9.8749 C41.6871&apos9.8749 40.9121&apos11.0089 40.8311&apos11.9999 L45.1131&apos11.9999 Z"
                 fill="#0a66c2"></path>
               <polygon fill="#0a66c2" points="38 8 34.5 8 31 12 31 3 28 3 28 18 31 18 31 13 34.699 18 38.241 18 34 12.533">
               </polygon>
               <path
-                d="M16,8 L18.827,8 L18.827,9.441 L18.858,9.441 C19.289,8.664 20.562,7.875 22.136,7.875 C25.157,7.875 26,9.792 26,12.45 L26,18 L23,18 L23,12.997 C23,11.525 22.469,10.5 21.227,10.5 C19.719,10.5 19,11.694 19,13.197 L19,18 L16,18 L16,8 Z"
+                d="M16&apos8 L18.827&apos8 L18.827&apos9.441 L18.858&apos9.441 C19.289&apos8.664 20.562&apos7.875 22.136&apos7.875 C25.157&apos7.875 26&apos9.792 26&apos12.45 L26&apos18 L23&apos18 L23&apos12.997 C23&apos11.525 22.469&apos10.5 21.227&apos10.5 C19.719&apos10.5 19&apos11.694 19&apos13.197 L19&apos18 L16&apos18 L16&apos8 Z"
                 fill="#0a66c2"></path>
               <path
-                d="M11,18 L14,18 L14,8 L11,8 L11,18 Z M12.501,6.3 C13.495,6.3 14.3,5.494 14.3,4.5 C14.3,3.506 13.495,2.7 12.501,2.7 C11.508,2.7 10.7,3.506 10.7,4.5 C10.7,5.494 11.508,6.3 12.501,6.3 Z"
+                d="M11&apos18 L14&apos18 L14&apos8 L11&apos8 L11&apos18 Z M12.501&apos6.3 C13.495&apos6.3 14.3&apos5.494 14.3&apos4.5 C14.3&apos3.506 13.495&apos2.7 12.501&apos2.7 C11.508&apos2.7 10.7&apos3.506 10.7&apos4.5 C10.7&apos5.494 11.508&apos6.3 12.501&apos6.3 Z"
                 fill="#0a66c2"></path>
               <polygon fill="#0a66c2" points="3 3 0 3 0 18 9 18 9 15 3 15"></polygon>
             </g>
           </svg> */}
-          <p className="text-3xl mt-12">Lest's Add in our Community, One step begin</p>
+          <p className="text-3xl mt-12">Lest's Add in our Community &apos One step begin</p>
           <div className="p-5 bg-white rounded-md w-96">
             <form className="space-y-4">
               <div className="space-y-0.5">
@@ -44,9 +44,9 @@ function Page() {
                 {/* <input className="block w-full p-1 border border-gray-500 rounded" type="password" id="password" aria-label="Password" required Length="6" /> */}
               </div>
               <p className="text-xs text-center text-gray-500">
-                By clicking Agree & Join, you agree to the LinkedIn
-                <a className="font-medium hover:underline text-linkedin" href="#">User Agreement,</a>
-                <a className="font-medium hover:underline text-linkedin" href="#">Privacy Policy,</a>
+                By clicking Agree & Join&apos you agree to the LinkedIn
+                <a className="font-medium hover:underline text-linkedin" href="#">User Agreement&apos</a>
+                <a className="font-medium hover:underline text-linkedin" href="#">Privacy Policy&apos</a>
                 and <a className="font-medium hover:underline text-linkedin" href="#">Cookie Policy</a>.
               </p>
               <button type="submit" className="w-full py-2 text-white rounded-full bg-linkedin hover:bg-linkedin-dark">Agree & Join</button>
